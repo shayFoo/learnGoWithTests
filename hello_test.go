4338b0d06e5376a972adb9ddadf6c28b46ca8bfe
shayfoo <oasis1975sun@gmail.com>
2026-04-03 11:59:49 +0000

    feat: handle empty string

--- hello_test.go
+++ hello_test.go
@@ -1,15 +1,27 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestHello(t *testing.T) {
-	name := "Chris"
-	got := Hello(name)
-	want := fmt.Sprintf("Hello, %s", name)
+	t.Run("saying hello to people", func(t *testing.T) {
+		name := "Chris"
+		got := Hello(name)
+		want := "Hello, " + name
 
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
