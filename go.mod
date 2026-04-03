ff92e6d6cee1e7ef79f25e37d87ec70cbf92f399
shayfoo <oasis1975sun@gmail.com>
2026-04-03 11:50:39 +0000

    initial-commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/shayfoo/learn-go-with-tests
+
+go 1.26.1
---
+++ hello_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestHello(t *testing.T) {
+	name := "Chris"
+	got := Hello(name)
+	want := fmt.Sprintf("Hello, %s", name)
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
